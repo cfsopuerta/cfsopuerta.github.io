1ed124ad217a608d1b5d50d57c21fda1aa518fd3
cfsopuerta <carlos.f.sopuerta@csic.es>
2025-05-02 12:06:44 +0200

    Initial commit of my website

---
+++ Carlos_F._Sopuerta_Webpage_at_ICE/News_&_Events_files/News_&_Events.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20090130
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('News_&_Events_files/News_&_EventsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
